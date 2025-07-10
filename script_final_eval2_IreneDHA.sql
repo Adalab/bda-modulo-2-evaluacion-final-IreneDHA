8a20975ee0f745318c650ab4e0534dc20dd7a3af
IreneDHA <irene.hoz.atero@gmail.com>
2025-07-10 11:13:19 +0200

    guardado hasta el ejercicio 10

--- script_final_eval2_IreneDHA.sql
+++ script_final_eval2_IreneDHA.sql
@@ -17,3 +17,34 @@ WHERE description LIKE "%amazing%";
 SELECT title AS películas_largas
 FROM film
 WHERE length > 120;
+
+-- EJER 5: Recupera los nombres de todos los actores.
+SELECT CONCAT(first_name, ' ', last_name) AS lista_actores
+FROM actor;
+
+-- EJER 6: Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido.
+SELECT CONCAT(first_name, ' ', last_name) AS actores_gibson
+FROM actor
+WHERE last_name LIKE 'gibson';
+
+-- EJER 7: Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20.
+SELECT CONCAT(first_name, ' ', last_name) AS lista_actores
+FROM actor
+WHERE actor_id BETWEEN 10 AND 20;
+
+-- EJER 8: Encuentra el título de las películas en la tabla film que no sean ni "R" ni "PG-13" en cuanto a su clasificación.
+SELECT title AS películas_aptas_para_niños
+FROM film 
+WHERE rating NOT IN ('r', 'PG-13');
+
+-- EJER 9: Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento.
+SELECT COUNT(title) AS num_películas, rating AS clasificación
+FROM film
+GROUP BY rating;
+
+-- EJER 10: Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.
+SELECT c.customer_id, CONCAT(first_name, ' ', last_name) AS clientes, COUNT(r.rental_id) AS num_películas_alquiladas
+FROM customer AS c
+INNER JOIN rental AS r -- hago este join porque solo me interesa ver información sobre clientes que han alquilado
+USING (customer_id)
+GROUP BY r.customer_id;
--- script_pruebas_eval2_IreneDHA.sql
+++ script_pruebas_eval2_IreneDHA.sql
@@ -14,3 +14,70 @@ SELECT title, length -- comprobración de que la selección se está haciendo co
 FROM film
 WHERE length > 120
 ORDER BY length ASC; -- comprobación de cuál es la película más cercana a 120 min
+
+-- EJER 5:
+SELECT DISTINCT first_name, last_name
+FROM actor;
+SELECT first_name, last_name
+FROM actor; -- al comprobar si hace falta el DISTINCT aparece 1 fila más
+
+SELECT first_name, last_name
+FROM actor
+WHERE first_name IS NULL OR last_name IS NULL; -- no parece haber ningún dato vacío
+
+SELECT actor_id
+FROM actor; -- con esta selección puedo ver que hay 200 actores ya que el actor_id es un dato único
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM actor; -- prueba para refinar el formato de concat
+
+-- EJER 6:
+SELECT first_name, last_name
+FROM actor
+WHERE last_name LIKE 'gibson';
+SELECT first_name, last_name
+FROM actor
+WHERE last_name LIKE '%gibson'; -- pensándolo he visto que en este caso si hay un apellido que sea por ejemplo McGibson esta opción también lo incluiría
+SELECT first_name, last_name
+FROM actor
+WHERE last_name IN ('gibson');
+
+-- EJER 7:
+SELECT first_name, last_name, actor_id 
+FROM actor
+WHERE actor_id BETWEEN 10 AND 20;
+
+-- EJER 8:
+SELECT title, rating
+FROM film 
+WHERE rating NOT IN ('r', 'PG-13')
+ORDER BY rating; -- he incluido esto porque me parece permitente tener las películas ordenadas por la condición que nos piden, pero no es necesario
+
+-- EJER 9:
+SELECT DISTINCT rating
+FROM film; -- comprobación de cuántas categorías hay en rating
+SELECT COUNT(title), rating
+FROM film
+GROUP BY rating;
+
+-- EJER 10:
+-- cantidad películas alquiladas x cliente
+-- mostrar id, nombre, apellido cliente + cantidad 
+SELECT * FROM inventory;
+SELECT * FROM rental;
+DESCRIBE inventory; -- no entendía muy bien qué era "inventory_id" y he descubierto esto gracias a CHATGPT
+-- con ello puedo ver el tipo de dato y el tipo de claves que hay en la tabla (PK o FK)
+SELECT * FROM customer AS c
+INNER JOIN rental AS r
+ON c.customer_id = r.customer_id; -- aquí veo las columnas que realmente necesito
+
+SELECT c.customer_id, r.customer_id, c.first_name, c.last_name, r.rental_id -- selección de columnas para comprobación
+FROM customer AS c
+INNER JOIN rental AS r
+ON c.customer_id = r.customer_id;
+
+SELECT c.customer_id, r.customer_id, c.first_name, c.last_name, COUNT(r.rental_id) -- selección de columnas para comprobación
+FROM customer AS c
+INNER JOIN rental AS r
+ON c.customer_id = r.customer_id -- para comprobar que el join se está haciendo correctamente
+GROUP BY r.customer_id;
